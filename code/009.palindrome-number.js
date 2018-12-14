2ce50a1de79734b7c02b3712b86694e7214221fb
keep <i@ickeep.com>
2018-12-14 13:03:58 +0800

    palindrome-number

--- code/009.palindrome-number.js
+++ code/009.palindrome-number.js
@@ -9,19 +9,32 @@ var isPalindrome = function (x) {
   if (x < 10) {
     return true
   }
-  const arr = (x + '').split('')
-  let is = true
-  const len = arr.length
-  for (i = 0; i < Math.ceil(len / 2); i += 1) {
-    const start = arr[i]
-    const end = arr[len - 1 - i]
-    if (start !== end) {
-      is = false
-      break
+  if (x % 10 === 0) {
+    return false
+  }
+  // const arr = (x + '').split('')
+  // let is = true
+  // const len = arr.length
+  // for (i = 0; i < Math.ceil(len / 2); i += 1) {
+  //   const start = arr[i]
+  //   const end = arr[len - 1 - i]
+  //   if (start !== end) {
+  //     is = false
+  //     break
+  //   }
+  // }
+  // return is
+  let y = 0
+  while (x > y) {
+    const tmp = x % 10
+    x = parseInt(x / 10, 10)
+    y = y * 10 + tmp
+    if (parseInt(x / 10, 10) === y) {
+      y = x
     }
   }
-  return is
+  return y === x
 }
 
-isPalindrome(1211)
-isPalindrome(121)
+isPalindrome(12321)
+isPalindrome(1221)
